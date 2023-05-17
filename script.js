75a6ca79d408e79d18e37be59777cac124f7153d
FUNnyManiac <andrejbosacenko@gmail.com>
2023-05-17 08:48:41 +0300

    feat: add finish 3 sections

---
+++ script.js
@@ -0,0 +1,7 @@
+// const sliderElems = document.querySelectorAll('.slide');
+// const slider = document.querySelector('.slider');
+// let currentPos = 0;
+// const previousSlide = document.querySelector('.previous_slide').addEventListener('click', function () {
+//     slider.style.transform = `translateX(${sliderElems[0]}px)`
+// })
+
